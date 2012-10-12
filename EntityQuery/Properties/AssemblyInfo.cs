cf88ada86afa451b155a0559b64fdb4ae58afa66
Eddy Recio <eddy@ngensoft.net>
2012-10-12 11:05:37 -0400

    Updated Assembly info

--- EntityQuery/Properties/AssemblyInfo.cs
+++ EntityQuery/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EntityQuery")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Linq Queryable for injecting application logic")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Eddy")]
 [assembly: AssemblyProduct("EntityQuery")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
